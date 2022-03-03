a96cfb6b8d50aa96369e9782924e515fd86cc0ba
Ravindra Kumar Meena <rmeena840@gmail.com>
2022-03-03 18:45:19 +0530

    index page added (#3)

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+	<a-row type="flex" justify="center">
+		<a-col :span="12">All rights reserved</a-col>
+	</a-row>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+	<a-row type="flex" justify="center">
+		<a-col :span="12">Wallet Connet</a-col>
+	</a-row>
+</template>
