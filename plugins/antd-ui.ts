21cbb23d9e43aa715585fdfb9bd99472e1d52f67
Ravinda <ravindra.iitism@gmail.com>
2022-03-03 12:21:01 +0530

    index page added

---
+++ plugins/antd-ui.ts
@@ -0,0 +1,7 @@
+import Antd from "ant-design-vue/lib";
+import { defineNuxtPlugin } from "#app";
+import "ant-design-vue/dist/antd.css";
+
+export default defineNuxtPlugin((nuxtApp) => {
+	nuxtApp.vueApp.use(Antd);
+});
