80e854478b165e36507c343d2e2278b840dc449c
Ravindra Kumar Meena <ravindra.iitism@gmail.com>
2022-03-22 16:49:01 +0530

    Use single wallet store in pinia (#24)

--- src/App.vue
+++ src/App.vue
@@ -6,44 +6,11 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import WalletStore from "./store/WalletStore";
-import { WalletType } from "./types";
-import { MAX_WALLET_COUNT_CHECK } from "./config/app.config";
 import AppHeader from "./components/AppHeader.vue";
 import AppFooter from "./components/AppFooter.vue";
-declare var AlgoSigner: any; // eslint-disable-line
 
 export default defineComponent({
 	name: "App",
 	components: { AppHeader, AppFooter },
-	data() {
-		return {
-			walletCheckCount: 1,
-		};
-	},
-	setup() {
-		const walletStore = WalletStore();
-
-		return {
-			setWalletType: walletStore.setWalletType,
-		};
-	},
-	mounted() {
-		this.checkAlgoSigner();
-	},
-	methods: {
-		async checkAlgoSigner() {
-			if (typeof AlgoSigner !== "undefined") {
-				this.setWalletType(WalletType.ALGOSIGNER);
-				await AlgoSigner.connect();
-			} else {
-				// maximum number of times to check wallet
-				if (this.walletCheckCount <= MAX_WALLET_COUNT_CHECK) {
-					this.walletCheckCount++;
-					setTimeout(() => this.checkAlgoSigner(), 500);
-				}
-			}
-		},
-	},
 });
 </script>
