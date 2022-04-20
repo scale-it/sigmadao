0ef4dee801230c623d590625b36fdf1c5774f738
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2022-04-21 03:15:33 +0530

    use algosigner indexer and algod client to get data

--- src/indexer/index.ts
+++ src/indexer/index.ts
@@ -1,20 +1,20 @@
-import algodClient from "@/config/algob.config";
+import { CHAIN_NAME } from "@/config/algosigner.config";
 import { GLOBAL_STATE, LOCAL_STATE } from "@/constants/constant";
 import {
 	Key,
 	StateValue,
 	AccountAddress,
 } from "@algo-builder/algob/build/types";
-import indexerClient from "../config/indexer.config";
+declare var AlgoSigner: any; // eslint-disable-line
 
 export const searchForAssets = async (
 	tokenName: string
 ): Promise<Record<string, any> | undefined> => {
 	try {
-		const accountInfo = await indexerClient
-			.searchForAssets()
-			.name(tokenName)
-			.do();
+		const accountInfo = await AlgoSigner.indexer({
+			ledger: CHAIN_NAME,
+			path: `/v2/assets?name=${tokenName}`,
+		});
 		return JSON.parse(JSON.stringify(accountInfo));
 	} catch (e) {
 		console.log(e);
@@ -25,9 +25,10 @@ export const searchForApplication = async (
 	application_id: number
 ): Promise<Map<string, StateValue> | undefined> => {
 	try {
-		const applicationInfo = await indexerClient
-			.lookupApplications(application_id)
-			.do();
+		const applicationInfo = await AlgoSigner.indexer({
+			ledger: CHAIN_NAME,
+			path: `/v2/applications/${application_id}`,
+		});
 
 		const creator = applicationInfo.application.params.creator;
 		const globalStateMap = await readAppState(
@@ -50,7 +51,10 @@ export const searchForAccount = async (
 	localStateMap: Map<string, StateValue> | undefined;
 }> => {
 	try {
-		const accountInfo = await indexerClient.lookupAccountByID(address).do();
+		const accountInfo = await AlgoSigner.indexer({
+			ledger: CHAIN_NAME,
+			path: `/v2/accounts/${address}`,
+		});
 		console.log(
 			"Information for Account: " + JSON.stringify(accountInfo, undefined, 2)
 		);
@@ -72,9 +76,11 @@ export async function readAppState(
 	account: AccountAddress,
 	appID: number
 ): Promise<Map<Key, StateValue> | undefined> {
-	const accountInfoResponse = await algodClient
-		.accountInformation(account)
-		.do();
+	const accountInfoResponse = await AlgoSigner.algod({
+		ledger: CHAIN_NAME,
+		path: `/v2/accounts/${account}`,
+	});
+
 	for (const app of accountInfoResponse[stateType]) {
 		if (app.id === appID) {
 			const stateMap = new Map<Key, StateValue>();
