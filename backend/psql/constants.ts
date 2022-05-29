671f4abd6bb28676dbf7823828fe728b625fb88e
Megha-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2022-05-29 19:04:59 +0530

    constants file removed

--- backend/psql/constants.ts
+++
@@ -1,5 +0,0 @@
-export const POSTGRES_UNAME = process.env.POSTGRES_UNAME || "algorand";
-export const POSTGRES_DBNAME = process.env.POSTGRES_DBNAME || "pgdb";
-export const POSTGRES_PASS = process.env.POSTGRES_PASS || "indexer";
-export const POSTGRES_HOST = process.env.POSTGRES_HOST || "localhost";
-export const POSTGRES_PORT = process.env.POSTGRES_PORT || 5432;
