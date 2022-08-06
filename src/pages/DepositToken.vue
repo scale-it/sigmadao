ae42320645633daf63c8d69d395b19e26003627b
Megha-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2022-08-06 22:56:24 +0530

    lint fix

--- src/pages/DepositToken.vue
+++ src/pages/DepositToken.vue
@@ -3,12 +3,12 @@
 		<a-col :span="12" :offset="6">
 			<p>
 				In order to vote for proposals you have to deposit your tokens. This
-				protects against double voting. Your token will be locked until then
-				end of the furthest proposal you voted (to be more specific, the token
-				will be locked until
-				<code>max({p \in proposal_you_voted; p.end_voting_end})</code>).
-				After that time you can withdraw your tokens, or keep them locked to
-				vote for other proposasl.
+				protects against double voting. Your token will be locked until then end
+				of the furthest proposal you voted (to be more specific, the token will
+				be locked until
+				<code>max({p \in proposal_you_voted; p.end_voting_end})</code>). After
+				that time you can withdraw your tokens, or keep them locked to vote for
+				other proposasl.
 			</p>
 		</a-col>
 	</a-row>
