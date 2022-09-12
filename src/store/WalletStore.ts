55eeb91e0ad02fde4be6db0c862b27aa37bb8790
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2022-09-12 13:32:20 +0530

    MyAlgo Wallet cross origin frame error fix (#81)
    * wallet fix
    
    * bug fix

--- src/store/WalletStore.ts
+++ src/store/WalletStore.ts
@@ -12,6 +12,8 @@ import {
 	openErrorNotificationWithIcon,
 	openSuccessNotificationWithIcon,
 } from "@/constants";
+import { shallowRef } from "vue";
+import { MyAlgoWalletSession } from "@algo-builder/web";
 
 export default defineStore("WalletStore", {
 	state: (): WalletStoreState => {
@@ -32,7 +34,15 @@ export default defineStore("WalletStore", {
 			this.walletKind = walletType;
 		},
 		setWebMode(webMode: WebModeTypes) {
-			this.webMode = webMode;
+			// my algo wallet has issue with ref in vue
+			// for reference : https://github.com/randlabs/myalgo-connect/issues/71
+			let myalgo = shallowRef() as any;
+			if (this.walletKind === WalletType.MY_ALGO) {
+				myalgo = webMode;
+				this.webMode = myalgo as MyAlgoWalletSession;
+			} else {
+				this.webMode = webMode;
+			}
 			console.log("WebMode Initialized", webMode);
 		},
 		setWalletAddress(address: string) {
