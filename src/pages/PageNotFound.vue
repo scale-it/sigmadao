861f4641a6b8ce3e55be32c4128826b52cb67c98
Ravindra Meena <ravindra.iitism@gmail.com>
2022-09-19 07:25:58 +0100

    Feature: 404 route handle with UI and redirection (#85)

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,34 @@
+<template>
+	<a-row type="flex" justify="center">
+		<a-col align="middle">
+			<img src="../assets/404.png" class="logo" />
+			<a-typography>
+				<a-typography-title>Sorry, Page Not Found</a-typography-title>
+			</a-typography>
+			<router-link :to="{ path: EndPoint.ALL_DAO }">
+				<a-button type="primary">
+					Go To Home Page
+					<template #icon><arrow-left-outlined /></template>
+				</a-button>
+			</router-link>
+		</a-col>
+	</a-row>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { EndPoint } from "@/types";
+import { ArrowLeftOutlined } from "@ant-design/icons-vue";
+
+export default defineComponent({
+	name: "PageNotFound",
+	components: {
+		ArrowLeftOutlined,
+	},
+	data() {
+		return {
+			EndPoint,
+		};
+	},
+});
+</script>
